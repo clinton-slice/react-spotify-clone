18c27fc30bbfa9090aee28b99ef1a66909bf62b7
clinton-slice <ccnkemdilim@gmail.com>
2020-11-16 18:56:58 -0500

    Fixed login bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import React from 'react';
 import Login from './screens/login';
+import { getTokenFromResponse } from './config/spotify';
 
 function App() {
+  const [token, setToken] = React.useState(null);
+  React.useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    // clear the access token for security reasons
+    window.location.hash = '';
+    const obtainedToken = hash.access_token;
+    if (obtainedToken) {
+      setToken(obtainedToken);
+    }
+    console.log('I HAVE TOKEN >>> ', token);
+  }, []);
   return (
     <div className="app">
-      <Login />
+      {token ? <div>Log in succesfull</div> : <Login />}
     </div>
   );
 }
