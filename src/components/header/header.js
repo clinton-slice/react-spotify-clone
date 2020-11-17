ddfb6247e15214d4ec53e0631562395229acd4ae
clinton-slice <ccnkemdilim@gmail.com>
2020-11-17 16:24:12 -0500

    Clean up header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
 import './header.css';
-import PropTypes from 'prop-types';
+
 import React from 'react';
 import { Avatar } from '@material-ui/core';
 // import SearchIcon from '@material-ui/icons/Search';
@@ -7,11 +7,9 @@ import { Avatar } from '@material-ui/core';
 import { useStateValue } from '../../wrappers/app-state-provider';
 import { Back, Forward } from '../../assets/icons';
 
-const Header = ({ spotify }) => {
-  const [{ user, playlists }] = useStateValue();
-  console.log('Header : ', spotify);
-  console.log('playlists : ', playlists);
-  // getplaylist(id, options, callback)
+const Header = () => {
+  const [{ user }] = useStateValue();
+
   return (
     <div className="header">
       <div className="header_left">
@@ -26,18 +24,10 @@ const Header = ({ spotify }) => {
 
       </div>
       <div className="header_right">
-        <div className="profile_black">
-          <Avatar alt={user?.display_name} src={user?.images[0].url} className="profile_avatar" />
-          <h4>{user?.display_name}</h4>
-        </div>
+        <Avatar alt={user?.display_name} src={user?.images[0].url} />
       </div>
     </div>
   );
 };
 
 export default Header;
-
-Header.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  spotify: PropTypes.object.isRequired,
-};
