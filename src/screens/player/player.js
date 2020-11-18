b94ce24e5d0a74d9626f213966d76763acad53cd
clinton-slice <ccnkemdilim@gmail.com>
2020-11-17 20:38:53 -0500

    Fix imports in player.js

--- src/screens/player/player.js
+++ src/screens/player/player.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Player.css';
 import PropTypes from 'prop-types';
-import Sidebar from '../../components/sidebar/Sidebar';
+import Sidebar from '../../components/sidebar/sidebar';
 import ControlBar from '../../components/controlbar/ControlBar';
-import Body from '../../components/body/Body';
+import Body from '../../components/body/body';
 
 const Player = ({ spotify }) => (
   <div className="player">
